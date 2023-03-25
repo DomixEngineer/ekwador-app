a80f3405ca846e06b43cf8a691517d425d49f3d3
DominikIT <Dominik.Dziewulski@wp.pl>
2023-03-25 12:52:26 +0100

    Photo gallery improved

---
+++ ekwador_app/src/mock/miniPhotoGallery.js
@@ -0,0 +1,36 @@
+let photos = [];
+photos['column_1'] = [
+    {
+        src: 'photo_1.png',
+        alt: 'Ekwador Manieczki'
+    },
+    {
+        src: 'photo_4.png',
+        alt: 'Ekwador Manieczki'
+    },
+]
+photos['column_2'] = [
+    {
+        src: 'photo_2.png',
+        alt: 'Ekwador Manieczki'
+    },
+    {
+        src: 'photo_5.png',
+        alt: 'Ekwador Manieczki'
+    }
+]
+photos['column_3'] = [
+    {
+        src: "photo_3.png",
+        alt: 'Ekwador Manieczki'
+    },
+    {
+        src: "photo_6.png",
+        alt: 'Ekwador Manieczki'
+    },
+    {
+        src: "photo_7.png",
+        alt: 'Ekwador Manieczki'
+    }
+]
+export default photos;
--- ekwador_app/src/page_parts/MiniPhotoGallery.vue
+++ ekwador_app/src/page_parts/MiniPhotoGallery.vue
@@ -3,35 +3,92 @@
         <div class="container">
             <section-header title="Galeria zdjęć z ostatnich imprez"></section-header>
         </div>
-        <div class="container photos-grid-container">
-            
+        <div class="container photos-container">
+            <div class="photos-container__column photos-container__column--first">
+                <img
+                    v-for="photo in photoGalleryList.column_1"
+                    :key="photo"
+                    :src="getImage(photo.src)" 
+                    alt="" 
+                    class="photos-container__column__single-photo"
+                >
+            </div>
+            <div class="photos-container__column photos-container__column--second">
+                <img
+                    v-for="photo in photoGalleryList.column_2"
+                    :key="photo"
+                    :src="getImage(photo.src)" 
+                    alt="" 
+                    class="photos-container__column__single-photo"
+                >
+            </div>
+            <div class="photos-container__column photos-container__column--third">
+                <img
+                    v-for="photo in photoGalleryList.column_3"
+                    :key="photo"
+                    :src="getImage(photo.src)" 
+                    alt="" 
+                    class="photos-container__column__single-photo"
+                >
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import SectionHeader from '@/components/SectionHeader.vue';
+import Photos from '../mock/miniPhotoGallery.js';
 
 export default {
     components: {
         SectionHeader
+    },
+    data() {
+        return {
+            // MOCK HTTP REQUEST RESPONSE
+            photoGalleryList: []
+        }
+    },
+    methods: {
+        getImage(img) {
+            return require('../assets/images/photo_gallery/' + img)
+        },
+        getPhotosGallery() {},
+    },
+    beforeMount() {
+        this.photoGalleryList = Photos;
+        console.log('Zdjecia', Photos.column_1)
     }
 }
 </script>
 
 <style lang="scss">
-.photos-grid-container {
+.photos-container {
     background-color: #21306f;
     width: 100%;
     margin-bottom: 25px;
     display: flex;
     flex-wrap: wrap;
-    // display: grid;
-    // grid-template-columns: repeat(3, 1fr);
-    // grid-column-gap: 30px;
     padding: 15px;
     box-sizing: border-box;
-    .single-img {
+    justify-content: space-between;
+    &__column {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 1100px;
+        &--first {
+            max-width: 421px;
+            width: 100%;
+        }
+        &--second {
+            max-width: 325px;
+            width: 100%;
+        }
+        &--third {
+            max-width: 326px;
+            width: 100%;
+        }
         img {
             display: block;
             width: 100%;
