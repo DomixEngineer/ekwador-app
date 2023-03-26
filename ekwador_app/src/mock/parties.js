1ed4cd1335e768abf2fe2ef8f3b57fc80f9e7d14
DominikIT <Dominik.Dziewulski@wp.pl>
2023-03-26 05:25:18 +0200

    Single party preview add image

--- ekwador_app/src/mock/parties.js
+++ ekwador_app/src/mock/parties.js
@@ -2,19 +2,19 @@ export default [
     {
         id: 1,
         title: 'Back to old night',
-        date: '09.04.2023r',
+        date: '09.04.2023',
         picture: 'bton_04_09_2023.png'
     },
     {
         id: 2,
         title: 'The history of Ekwador',
-        date: '11.03.2023r',
+        date: '11.03.2023',
         picture: 'the_history_03_11_2023.png'
     },
     {
         id: 3,
         title: '25 urodziny klubu',
-        date: '11.02.2023r',
+        date: '11.02.2023',
         picture: 'birthday_02_11_2023.png'
     },
 ]
--- ekwador_app/src/pages/PartyPreview.vue
+++ ekwador_app/src/pages/PartyPreview.vue
@@ -2,8 +2,42 @@
     <div class="top-header-bg">
     </div>
     <section-header :title="partyData.title + ' ' + partyData.date"></section-header>
-    <div class="container single-party-data">
-        <h1>{{ partyData.title }} {{ partyData.date }}</h1>
+    <div class="container">
+        <div class="single-party-data">
+            <h1 class="single-party-data__header">{{ partyData.title }} {{ partyData.date }}</h1>
+            <h3 class="single-party-data__subheader">do imprezy pozostało:</h3>
+            <div class="single-party-data__count">
+                <div class="single-party-data__count__single-element">
+                    <h1 class="single-party-data__count__single-element__main-value">17</h1>
+                    <h3 class="single-party-data__count__single-element__main-value-title">dni</h3>
+                </div>
+                <div class="single-party-data__count__single-element">
+                    <h1 class="single-party-data__count__single-element__main-value">12</h1>
+                    <h3 class="single-party-data__count__single-element__main-value-title">godzin</h3>
+                </div>
+                <div class="single-party-data__count__single-element">
+                    <h1 class="single-party-data__count__single-element__main-value">45</h1>
+                    <h3 class="single-party-data__count__single-element__main-value-title">minut</h3>
+                </div>
+            </div>
+            <h2 class="single-party-data__small-header">Opis wydarzenia</h2>
+            <p class="single-party-data__content-text">
+                "Sed ut perspiciatis unde omnis iste natus 
+                error sit voluptatem accusantium doloremque laudantium, 
+                totam rem aperiam, eaque ipsa quae ab illo inventore 
+                veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit 
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem 
+                ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non 
+                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam 
+                quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem 
+                ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+                Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil 
+                molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            </p>
+            <img :src="getPartyPoster()" :alt="partyData.title" class="single-party-data__party-poster" />
+        </div>
     </div>
     <upcoming-parties></upcoming-parties>
     <transport-cards></transport-cards>
@@ -36,6 +70,9 @@ export default {
             this.partyData = _.find(parties, (party) => {
                 return party.id == this.$route.params.party
             })
+        },
+        getPartyPoster() {
+            return require('../assets/images/party_posters/2023/' + this.partyData.picture)
         }
     },
     created() {
@@ -62,7 +99,65 @@ export default {
         top: 0;
     }
     .single-party-data {
-        @include background-opacity(#3b3b98, 0.4)
+        @include background-opacity(#3b3b98, 0.4);
+        padding: 15px;
+        box-sizing: border-box;
+        max-width: 811px;
+        &__header {
+            color: white;
+            font-family: 'lato';
+            font-weight: 300;
+            text-transform: uppercase;
+            margin: 0;
+            padding: 0;
+        }
+        &__subheader {
+            margin: 0;
+            padding: 0;
+            color: white;
+            font-family: 'lato';
+            font-weight: 300;
+            text-transform: uppercase;
+        }
+        &__small-header {
+            color: white;
+            font-family: 'lato';
+            text-transform: uppercase;
+            font-size: 35px;
+            font-weight: 800;
+        }
+        &__content-text {
+            color: white;
+            font-family: 'lato';
+            font-weight: 300;
+            font-size: 24px;
+            line-height: 1.4;
+        }
+        &__party-poster {
+            display: block;
+            width: 100%;
+        }
+        &__count {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            max-width: 220px;
+            &__single-element {
+                text-align: center;
+                margin-top: 20px;
+                &__main-value {
+                    color: white;
+                    font-family: 'lato';
+                    margin: 0;
+                }
+                &__main-value-title {
+                    color: white;
+                    font-family: 'lato';
+                    font-weight: 300;
+                    margin: 0;
+                }
+            }
+        }
     }
 
 </style>
